8411432745260a7f13e453dbd77aba875a9cd0af
Sirachon <maiphp@gmail.com>
2018-02-21 23:44:40 +0700

    delete member folder

--- member/resources/assets/js/components/Home.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="container">
-        Hello world
-    </div>
-</template>
--- member/resources/assets/js/components/Register.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="container">
-        Register form
-    </div>
-</template>
