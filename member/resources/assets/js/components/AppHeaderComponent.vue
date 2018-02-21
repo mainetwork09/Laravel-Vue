994602fd9d5e9ad1515a69988271fc851bf583f8
Mai <sirachon@ripplewerz.com>
2018-02-21 18:10:30 +0700

    first commit

---
+++ member/resources/assets/js/components/AppHeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="container">
+    <h1>{{ webName }}</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    data:function(){
+        return {
+            webName:'My Laravel-Vue'
+        }
+    }
+}
+</script>
